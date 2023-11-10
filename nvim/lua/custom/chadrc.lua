5a086c88bcb33d973c6518a6c52a4e2ce15939b5
spmfte <spam.fete@gmail.com>
2023-11-09 17:11:02 -0800

    chadrc.lua go brrrrr

--- nvim/lua/custom/chadrc.lua
+++ nvim/lua/custom/chadrc.lua
@@ -7,20 +7,96 @@ local highlights = require "custom.highlights"
 
 M.ui = {
   theme = "bearded-arc",
-  theme_toggle = { "bearded-arc", "bearded-arc" },
-  hl_override = highlights.override,
+  theme_toggle = { "bearded-arc", "catppuccin" },
+  hl_override = {
+     NvDashAscii = {
+      bg ="none",
+      fg ="blue"
+    },
+      NvDashButtons = {
+      bg = "none",
+      fg = "#abb7c1"
+    },
+  } ,
   hl_add = highlights.add,
+  transparency = false,
+  lsp_semantic_tokens = true,
+
+ -- https://github.com/NvChad/base46/tree/v2.0/lua/base46/extended_integrations
+  extended_integrations = {"notify"}, -- these aren't compiled by default, ex: "alpha", "notify"
+
+  -- cmp themeing
+  cmp = {
+    icons = true,
+    lspkind_text = true,
+    style = "atom_colored", -- default/flat_light/flat_dark/atom/atom_colored
+    border_color = "default", -- only applicable for "default" style, use color names from base30 variables
+    selected_item_bg = "colored", -- colored / simple
+  },
+
+  telescope = { style = "bordered" }, -- borderless / bordered
+
+  ------------------------------- nvchad_ui modules -----------------------------
+  statusline = {
+    theme = "vscode_colored", -- default/vscode/vscode_colored/minimal
+    -- default/round/block/arrow separators work only for default statusline theme
+    -- round and block will work for minimal theme only
+    separator_style = "default",
+    overriden_modules = nil,
+  },
+
+  -- lazyload it when there are 1+ buffers
+  tabufline = {
+    show_numbers = false,
+    enabled = true,
+    lazyload = true,
+    overriden_modules = nil,
+  },
+
+  -- nvdash (dashboard)
+  nvdash = {
+    load_on_startup = true,
+    transparency = false,
+
+    header = {
+      "         Aidan Littman           ",
+      "           ▄ ▄                   ",
+      "       ▄   ▄▄▄     ▄ ▄▄▄ ▄ ▄     ",
+      "       █ ▄ █▄█ ▄▄▄ █ █▄█ █ █     ",
+      "    ▄▄ █▄█▄▄▄█ █▄█▄█▄▄█▄▄█ █     ",
+      "  ▄ █▄▄█ ▄ ▄▄ ▄█ ▄▄▄▄▄▄▄▄▄▄▄▄▄▄  ",
+      "  █▄▄▄▄ ▄▄▄ █ ▄ ▄▄▄ ▄ ▄▄▄ ▄ ▄ █ ▄",
+      "▄ █ █▄█ █▄█ █ █ █▄█ █ █▄█ ▄▄▄ █ █",
+      "█▄█ ▄ █▄▄█▄▄█ █ ▄▄█ █ ▄ █ █▄█▄█ █",
+      "    █▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄█ █▄█▄▄▄█    ",
+    },
+
+    buttons = {
+      { "  Find File", "Spc f f", "Telescope find_files" },
+      { "󰈚  Recent Files", "Spc f o", "Telescope oldfiles" },
+      { "󰈭  Find Word", "Spc f w", "Telescope live_grep" },
+      { "  Bookmarks", "Spc m a", "Telescope marks" },
+      { "  Themes", "Spc t h", "Telescope themes" },
+      { "  Mappings", "Spc c h", "NvCheatsheet" },
+    },
+  },
+
+  cheatsheet = { theme = "grid" }, -- simple/grid
+
+  lsp = {
+    -- show function signatures i.e args as you type
+    signature = {
+      disabled = false,
+      silent = true, -- silences 'no signature help available' message from appearing
+    },
+  },
 }
 
--- Add the transparency option
-M.options = {
-    transparency = false
-}
+
 
 -- Add commands
 vim.cmd("command! Update NvChadUpdate")
 vim.cmd("command! Yy %y+")
-vim.cmd("command! Vimrc edit $MYVIMRC")
 
 -- Add current date and time
 vim.cmd("command! DateTime normal! i" .. os.date('%B %d, %Y @ %I:%M%p'))
